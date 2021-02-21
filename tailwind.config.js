fc89a99b2fd50e061f14cb52e935f48f1edc857b
CAPILLA19 <491710604@utaltamira.edu.mx>
2021-02-21 13:06:34 +0000

    final

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    fontfamily: {
+      bree: ['Bree Serif', 'serif'],
+      dancnig: ['Dancing Script', 'cursive'],
+      anton: ['Anton', 'sans-serif'],
+    },
+    extend: {
+      colors: {
+        danger: '#ff5f40',
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
